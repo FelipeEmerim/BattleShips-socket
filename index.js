8f9e1c66a1706bedd9fe9d7473975d3a19b01084
FelipeEmerim <lip.emerim@gmail.com>
2018-06-03 21:38:48 -0300

    add code comments

--- index.js
+++ index.js
@@ -1,32 +1,47 @@
-"use strict";
+"use strict"; //utiliza o modo estrito do JavaScript
 
-const express = require('express');
-const path = require('path');
-const ip = require('ip');
-const app = express();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
+//Para recuperar os elementos usar as posições de 0 a 9
+// matriz para controlar o que já foi destruído
+// 0 para célula intácta
+// 1 para barco
+// 2 para tiro na água
+// 3 tiro em um barco
 
-app.use(express.static('public'));
-app.get('/', function(req,res) {
-    res.sendFile(path.join(__dirname + '/public/Battleships.html'));
+
+const express = require('express'); //importa express
+const path = require('path'); //importa caminho
+const ip = require('ip');  //importa ip
+const app = express();    //inicializa express
+const http = require('http').Server(app); //cria servidor http, necessário para usar socket io
+const io = require('socket.io')(http); //inicializa e importa socket io
+
+app.use(express.static('public'));  //informa ao express o diretório de arquivos estáticos
+
+app.get('/', function(req,res) { //método que acontece quando cliente acessa a raiz
+    res.sendFile(path.join(__dirname + '/public/Battleships.html')); //carrega o html para o cliente
+    //_dirname = caminho até o projeto, importado de path
 });
 
 
 app.all('/*',function(req,res){
-    res.redirect('/');
+    res.redirect('/'); //redireciona para raiz em caso de qualquer outra rota
 
 });
 
-
-io.on('connection', function(socket){
-    console.log(socket.request.client._peername.address+" connected");
+/*O framework socket io permite a comunicação entre o servidor e o cliente através de eventos personalizados,
+tanto o servidor quando o cliente podem emitir e ouvir eventos, o nome do evento personalizado é o primeiro
+parametro da função on, isto configura um listener, ou seja, quem vai ouvir o evento. Para emitir um evento usa-se a
+função emit(), para isto gerar resultado, o listener do evento emitido precisa existir
+ */
+io.on('connection', function(socket){ //cria uma sessão para um cliente que se conecta
+    console.log(socket.request.client._peername.address+" connected"); //informa na console
+    //inicializa variáveis globais ao cliente
     let campo = [], cpuCampo = [];
     let hits, cpuHits;
     let cpuBoats = 9, userBoats = 9;
     const BOATS = 9;
 
-    function randomBoat(){
+    function randomBoat(){ //função que gera um barco aleatório com 5% de chance
         if(cpuBoats === 0) return 0;
 
         let rand = Math.random();
@@ -37,7 +52,7 @@ io.on('connection', function(socket){
         }else return 0;
     }
 
-    socket.on('cleanup', function () {
+    socket.on('cleanup', function () { //evento que inicializa as variaveis antes de o jogo começar
         hits = 0;
         cpuHits = 0;
         cpuBoats = 9;
@@ -56,7 +71,7 @@ io.on('connection', function(socket){
         ];
     });
 
-    socket.on('cpu-boats', function(){
+    socket.on('cpu-boats', function(){ //força a cpu a posicionar 9 barcos aleatoriamente
         while(cpuBoats > 0) {
             for (let i = 0; i < 10; i++) {
                 for (let j = 0; j < 10; j++) {
@@ -67,7 +82,7 @@ io.on('connection', function(socket){
         }
     });
 
-    socket.on('initialize-user', function(){
+    socket.on('initialize-user', function(){ //evento que reseta o campo do usuário
         campo = [
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -82,72 +97,76 @@ io.on('connection', function(socket){
         ];
     });
 
-    socket.on('set-boat', function(data){
-       if(campo[data.s_line][data.s_column] === 0){
+    socket.on('set-boat', function(data){ //evento que posiciona um barco do usuário, recebe as coordenadas em um JSON
+
+       if(campo[data.s_line][data.s_column] === 0){ //posição válida
            campo[data.s_line][data.s_column] = 1;
-           socket.emit('put-boat', {id:data.id});
-           userBoats--;
+           socket.emit('put-boat', {id:data.id}); //envia o id do elemento através de um JSON para o cliente
+           userBoats--; //diminui contador de barcos a posicionar
        }else{
-           socket.emit('stack-boat');
+           socket.emit('stack-boat'); //emite evento de posição inválida
        }
 
-       if(userBoats === 0){
-           socket.emit('game-start')
+       if(userBoats === 0){ //quando todos os barcos estiverem posicionados
+           socket.emit('game-start') //emite evento de começar o jogo
        }
     });
 
-    socket.on('user-shot', function(data){
+    socket.on('user-shot', function(data){ //evento que valida o tiro do usuario,
+        //recebe coordenadas e id do elemento através de JSON
         switch(cpuCampo[data.s_line][data.s_column]){
-            case 0:
+            case 0: //tiro na água
 
-                cpuCampo[data.s_line][data.s_column] = 2;
-                socket.emit('player-water-shot', {id: data.id});
+                cpuCampo[data.s_line][data.s_column] = 2; //seta celula para tiro na água
+                socket.emit('player-water-shot', {id: data.id}); //emite tiro na água para o cliente
                 break;
 
-            case 1:
-                cpuCampo[data.s_line][data.s_column] = 3;
-                hits++;
-                socket.emit('player-boat-shot', {id:data.id});
+            case 1: //tiro em barco
+                cpuCampo[data.s_line][data.s_column] = 3; //seta céula para tiro em barco
+                hits++; //aumenta o contador de acertos do cliente
+                socket.emit('player-boat-shot', {id:data.id}); //emite tiro em barco para o cliente
                 break;
 
-            default:
-                socket.emit('wasted-shot');
+            default: //tiro inválido
+                socket.emit('wasted-shot'); //emite evento de tiro inválido para o cliente
         }
     });
 
-    socket.on('cpu-shot', function (data) {
+    socket.on('cpu-shot', function (data) { //evento que controla o tiro da cpu
+//recebe um JSON com as coordenadas do tiro e a id da celula
 
         switch (campo[data.s_line][data.s_column]){
-            case 0:
-                campo[data.s_line][data.s_column] = 2;
-                socket.emit('cpu-water-shot', {id:data.id});
+            case 0://tiro na água
+                campo[data.s_line][data.s_column] = 2; //seta celula para tiro na agua
+                socket.emit('cpu-water-shot', {id:data.id}); //emite evento de tiro na agua para o cliente
                 break;
-            case 1:
-                campo[data.s_line][data.s_column] = 3;
-                cpuHits++;
-                socket.emit('cpu-boat-shot', {id:data.id});
+            case 1: //tiro em barco
+                campo[data.s_line][data.s_column] = 3; //seta celula para tiro em barco
+                cpuHits++; //aumenta o contador de acertos para a cpu
+                socket.emit('cpu-boat-shot', {id:data.id}); //emite evento de tiro em barco para o cliente
                 break;
-            default:
-                socket.emit('cpu-fail');
+            default: //tiro inválido
+                socket.emit('cpu-fail'); //emite evento de tiro inválido para o cliente
                 break;
+        }
+    }); //nos dois eventos de tiro SEMPRE é emitido a id do elemento para alterar ao cliente em caso de tiro válido
+
+    socket.on('game-state', function () { //evento que verifica o estado de jogo
+
+        if (hits === BOATS){ //se cliente venceu
+
+            socket.emit('player-win'); //emite evento de vitória do cliente ao cliente
+
+        }else if(cpuHits === BOATS){ //se cpu venceu
+            socket.emit('cpu-win'); //emite evento de vitória da cpu ao cliente
         }
     });
 
-    socket.on('game-state', function () {
-
-        if (hits === BOATS){
-
-            socket.emit('player-win');
-
-        }else if(cpuHits === BOATS){
-            socket.emit('cpu-win');
-        }
-    });
-
-    socket.on('disconnect', function () {
-        console.log(socket.request.client._peername.address+" disconnected")
+    socket.on('disconnect', function () { //quando o cliente se disconectar
+        console.log(socket.request.client._peername.address+" disconnected") //imprime na console
     })
 });
 
 
 http.listen(3000, () => console.log('BattleShips server online at ip '+ip.address()+' port 3000!'));
+//define porta em que o servidor vai ouvir e o inicializa, printa na console o ip do servidor e sua porta
--- public/BattleScript.js
+++ public/BattleScript.js
@@ -1,189 +1,193 @@
-"use strict";
+"use strict"; //usa o modo estrito do JavaScript
 
-//Para recuperar os elementos usar as posições de 0 a 9
-// matriz para controlar o que já foi destruído
-// 0 para célula intácta
-// 1 para barco
-// 2 para tiro na água
-// 3 tiro em um barco
+const myAudio = new Audio('background.mp3'); //inicializa variável de música do jogo
+myAudio.volume = 0.4; //deixa um volume suportável na música de fundo
+myAudio.loop = true; //configura o som para tocar em loop
+myAudio.play(); //toca música de fundo
+const container = document.getElementById("user"); //recupera o campo do usuário
+const cpu = document.getElementById("computer");  //recupera o campo da cpu
+const socket = io();  //inicializa o socket io no lado do cliente, isto foi importado no html
+container.style.pointerEvents = "none"; //desativa eventos no campo do usuário
 
-const myAudio = new Audio('background.mp3');
-myAudio.volume = 0.4;
-myAudio.loop = true;
-myAudio.play();
-const container = document.getElementById("user");
-const cpu = document.getElementById("computer");
-const socket = io();
-container.style.pointerEvents = "none";
+cleanup(); //roda função de inicializar o jogo
 
-cleanup();
+/*O framework socket io permite a comunicação entre o servidor e o cliente através de eventos personalizados,
+tanto o servidor quando o cliente podem emitir e ouvir eventos, o nome do evento personalizado é o primeiro
+parametro da função on, isto configura um listener, ou seja, quem vai ouvir o evento. Para emitir um evento usa-se a
+função emit(), para isto gerar resultado, o listener do evento emitido precisa existir
+ */
 
-function cleanup(){
-    container.innerHTML = '';
-    cpu.innerHTML = '';
+function cleanup(){ //função que inicializa o jogp
+    container.innerHTML = ''; //limpa o campo do cliente
+    cpu.innerHTML = '';  //limpa o campo da cpu
 
     container.className = 'container';
     cpu.className = 'container';
-    cpu.style.pointerEvents = 'none';
+    cpu.style.pointerEvents = 'none'; //desativa eventos do campo da cpu
 
 
 
-    socket.emit("cleanup");
+    socket.emit("cleanup"); //envia evento de inicializar o jogo ao servidor
 
-    socket.emit("cpu-boats");
+    socket.emit("cpu-boats");  //envia evento de posicionar barcos da cpu ao servidor
 
     for(let count = 0; count < 10; count++){
-        for(let count2 = 0; count2 < 10; count2++){
+        for(let count2 = 0; count2 < 10; count2++){ //cria o campo do usuário
             let element = document.createElement("div");
             element.setAttribute("class", "fieldCell");
-            element.setAttribute("id", "cell"+count+count2);
-            //element.addEventListener("click", setUserBoat);
-            element.setAttribute("data-line", count.toString());
-            element.setAttribute("data-column", count2.toString());
-            container.appendChild(element);
+            element.setAttribute("id", "cell"+count+count2); //gera id dinamico de acordo com as coordenadas
+            element.setAttribute("data-line", count.toString()); //cria atributo da linha
+            element.setAttribute("data-column", count2.toString()); //cria atributo da coluna
+            container.appendChild(element); //insere célula no container do usuário
         }
     }
 
     for(let count = 0; count < 10; count++){
-        for(let count2 = 0; count2 < 10; count2++){
+        for(let count2 = 0; count2 < 10; count2++){ //cria o campo da cpu
             let element = document.createElement("div");
             element.setAttribute("class", "fieldCellSelf");
-            element.setAttribute("id", "cpu"+count+count2);
-            element.addEventListener("click", setUserBoat);
-            element.setAttribute("data-line", count.toString());
-            element.setAttribute("data-column", count2.toString());
-            cpu.appendChild(element);
+            element.setAttribute("id", "cpu"+count+count2); //gera id dinamico de acordo com as coordenadas
+            element.addEventListener("click", setUserBoat); //adiciona o evento de colocar barco do usuário a célula
+            element.setAttribute("data-line", count.toString()); //cria atributo da linha
+            element.setAttribute("data-column", count2.toString()); //cria atributo da coluna
+            cpu.appendChild(element); //insere elemento no container da cpu
         }
 
     }
 
-    document.getElementById('start').style.display = 'block';
-    document.getElementById('restart').style.display = 'none';
+    document.getElementById('start').style.display = 'block'; //mostra o botão de começar o jogo
+    document.getElementById('restart').style.display = 'none'; //esconde o botão de reiniciar o jogo
 }
 
-function startGame(event){
+function startGame(event){ //função disparada
 
-    cpu.style.pointerEvents = 'auto';
-    requestUserBoats();
-    container.style.pointerEvents = "auto";
-    event.target.style.display = "none";
+    cpu.style.pointerEvents = 'auto'; //ativa eventos no campo da cpu
+    requestUserBoats(); //chama a função de pedir barcos do usuário
+    container.style.pointerEvents = "auto"; //ativa eventos no campo do usuário
+    event.target.style.display = "none"; //esconde o botão de começar o jogo
 }
 
-function setUserBoat(e){
-    let line = e.target.dataset.line;
-    let column = e.target.dataset.column;
-    let target = e.target;
+function setUserBoat(e){ //função que envia os dados de um barco do usuário a ser colocado ao servidor
+    let line = e.target.dataset.line; //recupera a coordenada da linha
+    let column = e.target.dataset.column;  //recupera a coordenada da coluna
 
-    socket.emit('set-boat', {s_line:line, s_column:column, id:target.id});
+    socket.emit('set-boat', {s_line:line, s_column:column, id:target.id}); //envia evento de colocar barco ao servidor
+    //envia um JSON com as coordenadas e a id do elemento ao servidor
 }
 
-socket.on('put-boat', function(data){
+socket.on('put-boat', function(data){ //evento que controla posicionamento de barco válido do usuário
     let target = document.getElementById(data.id);
     target.setAttribute('class', 'myBoat');
 });
 
-socket.on('stack-boat', function(){
+socket.on('stack-boat', function(){ //evento que controla posicionamento de barco inválido do usuário
     alert('Barcos não stackam');
 });
 
-socket.on('game-start',startGameplay);
+socket.on('game-start',startGameplay); //evento que começa a fase de tiros do jogo
 
-function startGameplay(){
+function startGameplay(){ //função que controla a fase de tiros do jogo
     let blocks = container.children;
     for(let i = 0; i < blocks.length; i++){
-        blocks[i].addEventListener("click", shot);
+        blocks[i].addEventListener("click", shot); //adiciona evento de tiro nas celulas do usuário
         blocks[i].className = 'fieldCellSelf';
     }
 
-    container.className ='container self';
+    container.className ='container self'; //destaca container do usuário
 
     let cpuBlocks = cpu.children;
     for(let i = 0; i < cpuBlocks.length; i++){
-        cpuBlocks[i].removeEventListener("click", setUserBoat);
+        cpuBlocks[i].removeEventListener("click", setUserBoat); //remove eventos de colocar barcos do campo da cpu
         if(cpuBlocks[i].className !== 'myBoat') cpuBlocks[i].className = 'fieldCell';
     }
 
-    cpu.className = 'container';
+    cpu.className = 'container'; //tira destaque do container da cpu
 
     alert("Que a batalha comece! Ataque o campo indicado!");
 }
 
-function requestUserBoats(){
-    socket.emit('initialize-user');
+function requestUserBoats(){ //função que recebe os barcos do usuários
+    socket.emit('initialize-user'); //emite evento para inicializar o campo do usuário ao servidor
 
-    cpu.className = 'container self';
+    cpu.className = 'container self'; //destaca o campo da cpu
 
-    alert("Clique nos blocos do campo indicado para posicionar 9 barcos");
+    alert("Clique nos blocos do campo indicado para posicionar 9 barcos"); //pede barcos do usuário
 }
 
-function shot(event) {
+function shot(event) { //função que controla os tiros realizados
 
-    play(event);
-    socket.emit('game-state');
+    play(event); //chama a função de tiro do usuário
+    socket.emit('game-state'); //verifica se há um vencedor
 }
 
-socket.on('player-win', function () {
-    window.alert("Você venceu");
-    container.style.pointerEvents = "none";
-    document.getElementById('restart').style.display = 'block';
+socket.on('player-win', function () { //evento que controla a vitoria do usuário
+    window.alert("Você venceu"); //informa o usuário de sua vitória
+    container.style.pointerEvents = "none"; //desativa eventos do campo do usuário
+    document.getElementById('restart').style.display = 'block'; //mostra o botão de recomeçar o jogo
 });
 
-socket.on('cpu-win', function () {
-    window.alert("Você perdeu");
-    container.style.pointerEvents = "none";
-    document.getElementById('restart').style.display = 'block';
+socket.on('cpu-win', function () { //evento que controla a vitoria da cpu
+    window.alert("Você perdeu"); //informa o usuário de sua derrota
+    container.style.pointerEvents = "none"; //desativa eventos do campo do usuário
+    document.getElementById('restart').style.display = 'block'; //mostra o botão de recomeçar o jogo
 });
 
-function explosion(){
+function explosion(){ //função que toca o som de explosão quando um barco é atingido
 	let audio = new Audio('long-explosion.mp3');
 	// noinspection JSIgnoredPromiseFromCall
     audio.play();
 }
 
-function cpuPlay(){
+function cpuPlay(){ //função que controla o tiro da cpu
 
     let line = Math.floor(Math.random()*10);
     let column = Math.floor(Math.random()*10);
+    //seleciona coordenadas aleatórias no campo
+
     let target = document.getElementById('cpu'+line+column);
+    //recupera o elemento escolhido
 
     socket.emit('cpu-shot', {s_line:line, s_column:column, id:target.id});
+    //envia evento de tiro da cpu ao servidor com JSON contendo coordenadas e id do elemento escolhido
 }
-socket.on('cpu-water-shot', function (data) {
+socket.on('cpu-water-shot', function (data) { //evento que controla tiro na água da cpu
     let target = document.getElementById(data.id);
     target.setAttribute("class", "shotWater");
 });
 
-socket.on('cpu-boat-shot', function (data) {
+socket.on('cpu-boat-shot', function (data) { //evento que controla tiro no barco da cpu
     let target = document.getElementById(data.id);
     target.setAttribute("class", "shotBoat");
-    explosion();
-    socket.emit('game-state');
+    explosion(); //emite som de explosão
+    socket.emit('game-state'); //verifica se houve vencedor
 });
 
-socket.on('cpu-fail', function () {
-    cpuPlay();
+socket.on('cpu-fail', function () { //evento que controla tiro inválido da cpu
+    cpuPlay(); //força cpu a dar um tiro válido
 });
 
-function play(event) {
-    let line = event.target.dataset.line;
-    let column = event.target.dataset.column;
-    let target = event.target;
+function play(event) { //função que controla o tiro do usuário
+    let line = event.target.dataset.line; //recupera linha do tiro
+    let column = event.target.dataset.column; //recupera coluna do tiro
+    let target = event.target; //recupera elemento do tio
     socket.emit('user-shot', {s_line:line, s_column:column, id:target.id});
+    //chama evento de tiro do servidor enviando JSON com coordenadas e id do elemento escolhido
 }
 
-socket.on('player-water-shot', function(data){
+socket.on('player-water-shot', function(data){ //evento que controla tiro na água do usuário
     let target = document.getElementById(data.id);
     target.setAttribute("class", "shotWaterSelf");
-    cpuPlay();
+    cpuPlay(); //chama tiro da cpu
 });
 
-socket.on('player-boat-shot', function (data) {
+socket.on('player-boat-shot', function (data) {  //evento que controla tiro no barco do usuário
     let target = document.getElementById(data.id);
     target.setAttribute("class", "shotBoatSelf");
-    explosion();
-    cpuPlay();
+    explosion(); //emite som de explosão
+    cpuPlay(); //chama tiro da cpu
 });
 
-socket.on('wasted-shot', function () {
-    window.alert("Pare de desperdiçar torpedos soldado");
+socket.on('wasted-shot', function () { //evento que controla tiro inválido do usuário
+    window.alert("Pare de desperdiçar torpedos soldado"); //informa o usuário do tiro inválido
+    //não chama tiro da cpu para possibilitar o usuário tentar seu tiro novamente
 });
